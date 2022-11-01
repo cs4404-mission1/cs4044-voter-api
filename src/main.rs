5d943d23d411bf58d57d0cad4ee1edcb6c06ca82
cannotilever <cannotilever@protonmail.com>
2022-10-31 22:29:31 -0400

    trying to impliment cookies

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
 #[macro_use] extern crate rocket;
 use rocket_dyn_templates::{Template, context};
-use rocket::form::{self, Form, Error};
-
+use rocket::{form::{self, Form}, http::{Cookie, CookieJar}, State};
+use rand_core::{RngCore, OsRng};
 #[derive(FromForm, Debug)]
 struct User<'r> {
-    #[field(validate = authuser(&self.password))]
     ssn: u64,
     password: &'r str,
 }
-
+// TODO: note for tomorrow: make a thread that hosts valid token vector in memory,
+// then make stateful channel to communicate with it to add and remove entries.
 #[get("/")]
 fn index() -> Template {
     Template::render("index",context! {})
 }
 
 #[post("/login", data = "<user>")]
-fn userlogon(user: Form<User<'_>>) -> Template{
-    println!("{:?}",user);
-    Template::render("authgood",context!{})
+fn userlogon(state: &State<Persist>, cookies: &CookieJar<'_>, user: Form<User<'_>>) -> Template{
+    println!("{:?}",&user);
+    let authok = true;
+    if authok{
+        let rndm = OsRng.next_u32().to_string();
+        cookies.add_private(Cookie::new("votertoken", rndm));
+        // REMOVE ME state.valid_tokens.push(rndm);
+    }
+    Template::render("auth",context!{authok})
 }
 
-fn authuser<'v>(ssn: &u64, password: &str) -> form::Result<'v, ()>{
-    // do database stuff
-    println!("SSN: {}, Password: {}",ssn,password);
-    match password{
-        "1234" => Ok(()),
-        _ => Err(Error::validation("Bad password"))?,
-    }
+#[get("/vote")]
+fn vote() -> Template {
+    Template::render("index",context! {})
 }
+
 
 #[catch(422)]
 fn invalid() -> Template {
-    Template::render("authfail",context! {})
+    Template::render("invalid",context! {})
+}
+
+struct Persist {
+    valid_tokens: Vec<String>,
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, userlogon]).register("/", catchers![invalid])
+    rocket::build().mount("/", routes![index, userlogon])
+    .register("/", catchers![invalid])
+    .manage(Persist {valid_tokens: vec!()})
     .attach(Template::fairing())
 }
